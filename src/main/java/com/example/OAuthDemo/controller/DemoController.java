74c6d1b2ac640fb15af8eecd3f11f455faacdd27
Rushi Baravkar <rushibaravkar1012@gmail.com>
2026-01-07 10:07:29 +0530

    updated

---
+++ src/main/java/com/example/OAuthDemo/controller/DemoController.java
@@ -0,0 +1,23 @@
+package com.example.OAuthDemo.controller;
+
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class DemoController {
+
+    @GetMapping("/public")
+    public String publicApi(){
+        return "Public API";
+    }
+
+    @GetMapping("/private")
+    public String privateApi(OAuth2AuthenticationToken token){
+        return "Private API" + token.getPrincipal().getAttribute("email");
+    }
+
+}
